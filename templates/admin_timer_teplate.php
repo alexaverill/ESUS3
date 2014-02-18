35a502578ae163239725f4f40cc612aa2ce7b269
Alex Averill <alex.averill2013@gmail.com>
2014-02-18 14:36:26 -0600

    Fixing timer template;w

--- templates/admin_timer_teplate.php
+++ templates/admin_timer_teplate.php
@@ -1,7 +1,7 @@
 
 <form method="post" action="">
-Start Date:<input id='datepicker' name='start_dt' class='datepicker'><input id='start_time' name='start_time' class='timepicker'/><br/>
-End Date:<input id='enddatepicker' name='end_dt' class='myclass datepicker'><input id='end_time' name='end_time' class='myclass timepicker'/><br/>
+Start Date:<input id='datepicker' name='start_dt' class='datepicker'> Time:<input id='start_time' name='start_time' class='timepicker'/><br/>
+End Date:<input id='enddatepicker' name='end_dt' class='myclass datepicker'> Time:<input id='end_time' name='end_time' class='myclass timepicker'/><br/>
 <input type="submit" value="Update Timer" name="update"/>
  </form>
 <script>
