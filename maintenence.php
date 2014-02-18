56f8c6e76253737fe6fe285317fc3f39ce263173
Alex Averill <alex.averill2013@gmail.com>
2014-02-18 15:30:31 -0600

    Adding maintenence file outline

---
+++ maintenence.php
@@ -0,0 +1,12 @@
+<?php
+//Basic maintenence file.
+
+//Clear databases (pull install.sql and just clear and reset database
+
+//Check # of slots vs max possible in database
+//if not equal expand database
+
+//Clear teams
+
+//Other goodies as needed
+?>
