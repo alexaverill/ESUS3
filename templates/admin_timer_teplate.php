31c8b6e98e773eac820ddd9fd72665fa32508d47
alexaverill <alex.averill201@gmail.com>
2014-02-27 21:17:01 -0600

    Datepicker is being a pain

--- templates/admin_timer_teplate.php
+++ templates/admin_timer_teplate.php
@@ -1,7 +1,7 @@
 <h2>Change Timer</h2>
 <form method="post" action="">
-Start Date:<input id='datepicker' name='start_dt'  id="dpd1"> Time:<input id='start_time' name='start_time' class='timepicker'/><br/>
-End Date:<input id='enddatepicker' name='end_dt'  id="dpd2"> Time:<input id='end_time' name='end_time' class='myclass timepicker'/><br/>
+Start Date:<input id='dpd1' name='start_dt' > Time:<input id='start_time' name='start_time' class='timepicker'/><br/>
+End Date:<input id='dpd2' name='end_dt' > Time:<input id='end_time' name='end_time' class='myclass timepicker'/><br/>
 <input type="submit" value="Update Timer" name="update"/>
  </form>
 <!--<script>
