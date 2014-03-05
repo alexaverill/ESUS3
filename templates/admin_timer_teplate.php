931b993d33f30f1f34c0bc519c6cfdc6ca36f035
alexaverill <alex.averill201@gmail.com>
2014-03-04 22:17:49 -0600

    More options to timer page.

--- templates/admin_timer_teplate.php
+++ templates/admin_timer_teplate.php
@@ -4,6 +4,15 @@ Start Date:<input id='dp1' name='start_dt' type="text"> Time:<input id='start_ti
 End Date:<input id='dp2' name='end_dt' > Time:<input id='end_time' name='end_time' class='myclass timepicker'/><br/>
 <input type="submit" value="Update Timer" name="update"/>
  </form>
+<h2>Change Method</h2>
+<form method="POST" action="">
+    Method:<select name="type">
+        <option value="1">Rely on Timer</option>
+        <option value="1">Open</option>
+        <option value="1">Closed</option>
+    </select>
+    
+</form>
 <script>
     $.datepicker.formatDate("yy-mm-dd");
       $(function() {
