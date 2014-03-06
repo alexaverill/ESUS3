c68460769d45c127c663c0af9fe75e1524b84910
alexaverill <alex.averill201@gmail.com>
2014-03-05 20:09:36 -0600

    Adding sql for cleaning database

--- maintenence.php
+++ maintenence.php
@@ -2,11 +2,15 @@
 //Basic maintenence file.
 
 //Clear databases (pull install.sql and just clear and reset database
+$clear_times="DELETE * FROM `times`";
+$clear_team="DELETE * FROM `teams`";
+$clear_slots="DELETE * FROM `slots`";
+$clear_event="DELETE * FROM `event`";
 
 //Check # of slots vs max possible in database
 //if not equal expand database
 
 //Clear teams
-
+$clear_sql="DELETE * FROM `teams`";
 //Other goodies as needed
 ?>
