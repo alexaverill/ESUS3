d3c6da5571985f29642a829c66ae2ec5a7f757b2
Alex <alex.averill2013@gmail.com>
2014-04-05 22:21:38 -0500

    Fixing missing refresh when adding time slots. As well as the massive refresh when using multi add time

--- admin_adding.php
+++ admin_adding.php
@@ -18,15 +18,19 @@ if($Verify->is_admin()){
         if($_POST['add_slot']){
             $SLOTS->add_slots($_POST['time_slot']);
             //add_time_slot();
-            foreach($_POST['check_list'] as $check) {      //Loop through checked events to add to time slot.
-               if($EVENTS->add_events_at($check,$_POST['time_slot'])==0){
-                    echo 'Slot Added';
-                    $MVC->display_admin_adding();
-               }else{
-                    echo 'There was an error adding slots. Please try again.';
-                    $MVC->display_admin_adding();
-               }
+            if(isset($_POST['check_list'])){
+                foreach($_POST['check_list'] as $check) {      //Loop through checked events to add to time slot.
+                   if($EVENTS->add_events_at($check,$_POST['time_slot'])==0){
+                        echo 'Slot Added';
+                   }else{
+                        echo 'There was an error adding slots. Please try again.';
+                   }
+                }
+                $MVC->display_admin_adding();
+            }else{
+                $MVC->display_admin_adding();
             }
+            
         }
         if($_POST['add_times']){
             foreach($_POST['time_checks'] as $time){
