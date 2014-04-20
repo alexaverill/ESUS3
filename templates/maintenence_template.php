5ccc18fddc90caf0c5b66c368d758c364cff5a2f
Alex <alex.averill2013@gmail.com>
2014-04-20 10:57:48 -0500

    New files for templating

---
+++ templates/maintenence_template.php
@@ -0,0 +1,16 @@
+<h1>Cleaning Functions:</h1>
+   <form method="POST" action="">
+    <h2>Clear Times</h2><input type="submit" name="times" value="Clear Times"/>
+</form>
+
+<form method="POST" action="">
+    <h2>Clear Teams</h2><input type="submit" name="teams" value="Clear Teams"/>
+</form>
+
+<form method="POST" action="">
+    <h2>Clear Slots</h2><input type="submit" name="slots" value="Clear Slots"/>
+</form>
+
+<form method="POST" action="">
+    <h2>Clear Events</h2><input type="submit" name="events" value="Clear Events"/>
+</form>
