07ea643208bd13728df8d6955cb42bcd2cd24c7b
Alex <alex.averill2013@gmail.com>
2014-04-22 16:24:47 -0500

    maintenence admin verify

--- maintenence.php
+++ maintenence.php
@@ -1,20 +1,17 @@
 <?php
 include('header.php');
 //Basic maintenence file.
-$MVC=new MVC();
-$MVC->display_log();
-
-$MVC->display_maintenence();
-//Clear databases (pull install.sql and just clear and reset database
-$clear_times="DELETE * FROM `times`";
+$verify = new Verification;
+if($verify->is_admin()){
+    $MVC=new MVC();
+    $MVC->display_log();
+    $MVC->display_maintenence();
+    //Clear databases (pull install.sql and just clear and reset database
+    $clear_times="DELETE * FROM `times`";
 $clear_team="DELETE * FROM `teams`";
 $clear_slots="DELETE * FROM `slots`";
 $clear_event="DELETE * FROM `event`";
-
-//Check # of slots vs max possible in database
-//if not equal expand database
-
-//Clear teams
 $clear_sql="DELETE * FROM `teams`";
-//Other goodies as needed
+
+}
 ?>
