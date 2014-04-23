0f45ecfe2fc27c0f524d856ce0534ca76514b92f
Alex <alex.averill2013@gmail.com>
2014-04-22 21:08:37 -0500

    Testing

--- maintenence.php
+++ maintenence.php
@@ -3,15 +3,15 @@ include('header.php');
 //Basic maintenence file.
 $verify = new Verification;
 if($verify->is_admin()){
-    $MVC=new MVC();
+    //$MVC=new MVC();
     $MVC->display_log();
     $MVC->display_maintenence();
     //Clear databases (pull install.sql and just clear and reset database
     $clear_times="DELETE * FROM `times`";
-$clear_team="DELETE * FROM `teams`";
-$clear_slots="DELETE * FROM `slots`";
-$clear_event="DELETE * FROM `event`";
-$clear_sql="DELETE * FROM `teams`";
+    $clear_team="DELETE * FROM `teams`";
+    $clear_slots="DELETE * FROM `slots`";
+    $clear_event="DELETE * FROM `event`";
+    $clear_sql="DELETE * FROM `teams`";
 
 }
 ?>
