52e46abfc98372ace3b9c29edc6553e625f58a7d
Alex <alex.averill2013@gmail.com>
2014-05-04 16:24:41 -0500

    Seperators

--- templates/admin_mail_template.php
+++ templates/admin_mail_template.php
@@ -1,9 +1,12 @@
 <?php $MVC=new MVC();?>
 <p>This page allows you to send teams their event times via the email that you have for them</p>
     <h2>Send Teams Their Times</h2>
+  <hr>
     <?php echo $MVC->display_email_options(1);?>
+    <hr>
     <h2>Send All Teams Their Times</h2>
-    <form method="POST" action=""><input name="all_teams" type="submit" value="Send All Teams Times"/></form> 
+    <form method="POST" action=""><input name="all_teams" type="submit" value="Send All Teams Times"/></form>
+    <hr>
    <h2>Send All Teams Their Passwords</h2>
    <h4>How your Excel sheet (.xls) should look: <a href="source/example.xls">Download Example</a></h4>
    <h4><a href="admin_em_example.php">Email Template</a></h4>
@@ -18,12 +21,13 @@
    });
    </script>
 
-
+<hr>
     <h2>Announcements</h2>
     <p> Send an email announcement to all teams
     <form method="POST" action="">
     <textarea name="emessage" id="emess" height="100px" width="15%"></textarea><br/>
     <input name="send_ann" type="submit" value="Send Announcement"/>
     </form>
+    <hr>
     <h2>Send Mail to a Team</h2>
     <?php echo $MVC->display_email_options(2);?>
