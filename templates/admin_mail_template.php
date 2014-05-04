c6dc0eed75738a462b41069d242a39a9bdabc77d
Alex <alex.averill2013@gmail.com>
2014-05-04 16:17:03 -0500

    Button and style fixes

--- templates/admin_mail_template.php
+++ templates/admin_mail_template.php
@@ -3,12 +3,13 @@
     <h2>Send Teams Their Times</h2>
     <?php echo $MVC->display_email_options(1);?>
     <h2>Send All Teams Their Times</h2>
-    <form method="POST" action=""><input name="all_teams" type="submit" class="myButton" value="Send All Teams Times"/></form> 
+    <form method="POST" action=""><input name="all_teams" type="submit" value="Send All Teams Times"/></form> 
    <h2>Send All Teams Their Passwords</h2>
-   <h4>How your Excel sheet (.xls) should look:<a href="source/example.xls">Download Example</a></h4>
+   <h4>How your Excel sheet (.xls) should look: <a href="source/example.xls">Download Example</a></h4>
    <h4><a href="admin_em_example.php">Email Template</a></h4>
    <form enctype="multipart/form-data" action="" method="POST">
-    Message:<textarea name="message" id="emess" height="100px" width="15%"></textarea><br/><input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+    Message:
+    <br/><textarea name="message" id="emess" height="100px" width="15%"></textarea><br/><input type="hidden" name="MAX_FILE_SIZE" value="100000" />
     Choose a sheet to upload: <input name="uploadedfile" id="file" type="file" /><br/>
     <input type="submit" name="up" value="Send Bulk Passwords" />
     </form>
@@ -22,7 +23,7 @@
     <p> Send an email announcement to all teams
     <form method="POST" action="">
     <textarea name="emessage" id="emess" height="100px" width="15%"></textarea><br/>
-    <input name="send_ann" type="submit" class="myButton" value="Send Announcement"/>
+    <input name="send_ann" type="submit" value="Send Announcement"/>
     </form>
     <h2>Send Mail to a Team</h2>
     <?php echo $MVC->display_email_options(2);?>
