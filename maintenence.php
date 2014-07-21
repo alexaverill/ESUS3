9ea13a689df2d24b9012e59291fbf6cd970213f6
Alex <alex.averill2013@gmail.com>
2014-07-20 22:07:42 -0500

    Style, and logging

--- maintenence.php
+++ maintenence.php
@@ -2,6 +2,7 @@
 include('header.php');
 //Basic maintenence file.
 $verify = new Verification;
+$log = new Logging;
 if($verify->is_admin()){
     $MVC->display('admin_maintenence_template.php');
     //Clear databases (pull install.sql and just clear and reset database
@@ -13,16 +14,20 @@ if($verify->is_admin()){
     $clear_sql="DELETE FROM `teams`";
     if($_POST['times']){
         $dbh->query($clear_times);
+        $log->add_entry('Admin','Cleared all times');
     }
     if($_POST['teams']){
         $dbh->query($clear_team);
+        $log->add_entry('Admin','Cleared all teams');
     }
     if($_POST['slots']){
         $dbh->query($clear_slots);
+        $log->add_entry('Admin','Cleared all slots');
         
     }
     if($_POST['events']){
         $dbh->query($clear_event);
+        $log->add_entry('Admin','Cleared all events');
     }
 }
 ?>
--- templates/login.php
+++ templates/login.php
@@ -1,7 +1,7 @@
 <h1>Login</h1>
 		<div class="user_info">
 			<form method="POST" action="">
-				<label for="user">Username</label><input type="text" name="user" size="25"/><br/>
+				<label for="user">Username<input type="text" name="user" size="25"/></label><br/>
 				Password<input type="password" name="pass" size="25"/><br/>
 				<input class="myButton" value="Login" type="submit" />
 				<input type=hidden name=submitted value="1"> 
