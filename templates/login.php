f3e365d7cce63b833ed47c0fa1c38fed530a0eec
Alex <alex.averill2013@gmail.com>
2014-07-20 22:08:38 -0500

    Login page layout

--- templates/login.php
+++ templates/login.php
@@ -1,8 +1,8 @@
 <h1>Login</h1>
 		<div class="user_info">
 			<form method="POST" action="">
-				<label for="user">Username<input type="text" name="user" size="25"/></label><br/>
-				Password<input type="password" name="pass" size="25"/><br/>
+				Username:<input type="text" name="user" size="25"/><br/>
+				Password:<input type="password" name="pass" size="25"/><br/>
 				<input class="myButton" value="Login" type="submit" />
 				<input type=hidden name=submitted value="1"> 
 			</form>
