1da6d5982f881b1e16ad6679dee6a564b8391194
Alex <alex.averill2013@gmail.com>
2014-07-23 16:01:17 -0500

    Change user menu

--- templates/user_menu.php
+++ templates/user_menu.php
@@ -1,5 +1,4 @@
-<ul>
+
 <li><a href="index.php" class="menu_link">Select Event Times</a></li>
 <li><a href="view.php" class="menu_link">View Your Event Times</a></li>
 <li><a href="logout.php" class="menu_link">Log Out</a></li>
-</ul>
