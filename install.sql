5db213d44126a34b2d9e5fe329abea4aedf1ffcb
Alex <alex.averill2013@gmail.com>
2014-07-31 13:33:47 -0500

    Fixing SQL

--- install.sql
+++ install.sql
@@ -23,49 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `beds`
---
-
-CREATE TABLE IF NOT EXISTS `beds` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `team_id` varchar(11) NOT NULL,
-  `first` varchar(100) NOT NULL,
-  `last` varchar(100) NOT NULL,
-  `gender` varchar(10) NOT NULL,
-  `arrive` int(11) NOT NULL,
-  `depart` int(11) NOT NULL,
-  `linens` varchar(10) NOT NULL,
-  `occ` varchar(10) NOT NULL,
-  `disability` varchar(10) NOT NULL,
-  `role` varchar(10) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `paid` int(11) NOT NULL,
-  `type` int(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=532 ;
-
---
--- Dumping data for table `beds`
---
-
-INSERT INTO `beds` (`id`, `team_id`, `first`, `last`, `gender`, `arrive`, `depart`, `linens`, `occ`, `disability`, `role`, `date`, `paid`, `type`) VALUES
-(519, '?', '?', '?', '?', 1, 1, '?', '?', '?', '?', '2014-04-23 15:33:38', 0, 0),
-(520, '?', '?', '?', '?', 0, 0, '?', '?', '?', '?', '2014-04-23 15:35:00', 0, 0),
-(521, '?', '?', '?', '?', 0, 0, '?', '?', '?', '?', '2014-04-23 15:35:24', 0, 0),
-(522, '31', '', '', '', 0, 0, '', '', 'no', '', '2014-04-25 14:59:34', 0, 0),
-(523, '31', '', '', '', 0, 0, '', '', '', '', '2014-04-25 01:16:05', 0, 0),
-(524, '31', 'adfda', 'fadfdsa', 'm', 14, 14, '0', '1', 'yes', 'competitor', '2014-04-25 14:59:45', 0, 0),
-(525, '31', 'LAeadadfdas', '', 'm', 15, 15, '0', '2', 'yes', 'competitor', '2014-04-25 14:59:50', 0, 0),
-(526, '31', 'LAeadadfdas', '', 'm', 15, 15, '0', '2', 'no', 'competitor', '2014-04-25 01:28:11', 0, 0),
-(527, '31', 'fafdafd', 'fdsfdsafdsa', 'm', 14, 14, '0', '2', 'no', 'competitor', '2014-04-25 01:29:03', 0, 0),
-(528, '31', 'fadsfdsa', 'fdasfdsaf', 'm', 14, 14, '0', '2', 'no', 'competitor', '2014-04-25 01:38:25', 0, 0),
-(529, '31', 'adfafdsa', 'fdsafdsafdsa', 'm', 14, 14, '0', '2', 'no', 'competitor', '2014-04-25 14:56:35', 0, 0),
-(530, '40', 'Test', 'test', 'm', 14, 14, '0', '2', 'no', 'competitor', '2014-05-06 18:11:54', 0, 0),
-(531, '40', 'test', 'tsaet', 'm', 14, 14, '0', '2', 'no', 'competitor', '2014-05-06 18:11:54', 0, 0);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `data`
 --
 
@@ -113,13 +70,6 @@ CREATE TABLE IF NOT EXISTS `event` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=29 ;
 
---
--- Dumping data for table `event`
---
-
-INSERT INTO `event` (`id`, `event`, `slots`) VALUES
-(28, 'testing', 10);
-
 -- --------------------------------------------------------
 
 --
@@ -161,32 +111,6 @@ CREATE TABLE IF NOT EXISTS `members` (
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`id`, `rank`, `name`, `password`, `email`, `perm`) VALUES
-(550, 1, 'admin', '$2y$10$zmlZ2wknu7kYZD/p2VXhZ.JV3/jR4aq8Q44pbLPiNRmGDXQLJAoKK', 'admin', 0),
-(550, 0, 'admin2', '$2y$10$.ZZCbbWOHo3d2ndqhNh/..D5LU.v3zobs/VCqZqHjKI.90r8YeToa', '', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `school_data`
---
-
-CREATE TABLE IF NOT EXISTS `school_data` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `team_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `number` varchar(10) NOT NULL,
-  `address` varchar(1000) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `state` varchar(100) NOT NULL,
-  `zipcode` int(5) NOT NULL,
-  `total_pay` int(10) NOT NULL,
-  `total_to_pay` int(20) NOT NULL,
-  `paid` int(10) NOT NULL,
-  `type` int(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
 -- --------------------------------------------------------
 
 --
@@ -202,25 +126,7 @@ CREATE TABLE IF NOT EXISTS `settings` (
 -- Dumping data for table `settings`
 --
 
-INSERT INTO `settings` (`tables`, `install`) VALUES
-(10, 3933);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `signatures`
---
-
-CREATE TABLE IF NOT EXISTS `signatures` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `team_number` int(11) NOT NULL,
-  `coach_name` varchar(15) NOT NULL,
-  `coach_number` int(11) NOT NULL,
-  `coach_email` varchar(50) NOT NULL,
-  `prince_name` varchar(15) NOT NULL,
-  `prince_sig` varchar(15) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -250,12 +156,6 @@ CREATE TABLE IF NOT EXISTS `team` (
   KEY `username` (`username`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=42 ;
 
---
--- Dumping data for table `team`
---
-
-INSERT INTO `team` (`id`, `name`, `email`, `username`, `password`) VALUES
-(41, 'Solon High', 'alex.averill2013@gmail.com', 'solon', '$2y$10$Evq64KrTfV45Bj.qfFcZyepOUe0KbBM8GCabMceeTt2aIIZ90984W');
 
 -- --------------------------------------------------------
 
