dd9e451ba08ffc5b65edd87293bb5234973afe0b
Alex <alex.averill2013@gmail.com>
2014-08-07 23:46:48 -0500

    danger will robinson buttons

--- templates/admin_maintenence_template.php
+++ templates/admin_maintenence_template.php
@@ -2,17 +2,17 @@
 <iframe name=my_frame src=logging.txt height=300px width=100%></iframe>
 <h1>Cleaning Functions:</h1>
    <form method="POST" action="">
-    <h2>Clear Times</h2><input type="submit" name="times" value="Clear Times"/>
+    <h2>Clear Times</h2><input type="submit" class="btn btn-danger" name="times" value="Clear Times"/>
 </form>
 
 <form method="POST" action="">
-    <h2>Clear Teams</h2><input type="submit" name="teams" value="Clear Teams"/>
+    <h2>Clear Teams</h2><input type="submit" class="btn btn-danger" name="teams" value="Clear Teams"/>
 </form>
 
 <form method="POST" action="">
-    <h2>Clear Slots</h2><input type="submit" name="slots" value="Clear Slots"/>
+    <h2>Clear Slots</h2><input type="submit" class="btn btn-danger" name="slots" value="Clear Slots"/>
 </form>
 
 <form method="POST" action="">
-    <h2>Clear Events</h2><input type="submit" name="events" value="Clear Events"/>
+    <h2>Clear Events</h2><input type="submit"  class="btn btn-danger" name="events" value="Clear Events"/>
 </form>
